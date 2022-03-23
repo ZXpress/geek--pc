3853697494b476591fb335fe2e638246a007b3c2
zcy <1832244088@qq.com>
2022-03-23 14:34:49 +0800

    登录功能-基本结构

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// 导入antd的全局样式
+import 'antd/dist/antd.css'
+
 import './index.css'
 import App from './App'
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 
 export default class Login extends Component {
   render() {
-    return <div>Login</div>
+    return (
+      <div className="login">
+        <Card className="login-container">
+          <img className="login-logo" src={logo} alt="" />
+        </Card>
+      </div>
+    )
   }
 }
