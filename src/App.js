3b9c434e39a476c0f7bc7339e32339258182d1f1
zcy <1832244088@qq.com>
2022-03-23 09:46:30 +0800

    项目配置路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Layout/index'
+import Login from './pages/Login/index'
 function App() {
-  return <div className="App">App组件</div>
+  return (
+    <Router>
+      <div className="App">
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
+
+        {/* 配置路由规则 */}
+        <Switch>
+          <Route path="/home" component={Home}></Route>
+          <Route path="/login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
