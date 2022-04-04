6a95dc2c54d781d93c5e09b781f2e428ca76d9bc
Xpress <1832244088@qq.com>
2022-04-04 19:43:08 +0800

    按需加载

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import {
   Switch,
   Redirect,
 } from 'react-router-dom'
-import Home from './pages/Layout/index'
-import Login from './pages/Login/index'
+import React, { Suspense } from 'react'
+import history from 'utils/history'
 import AuthRoute from 'components/AuthRoute/index'
 
-import history from 'utils/history'
+// import Home from './pages/Layout/index'
+// import Login from './pages/Login/index'
+
+// 懒加载
+const Login = React.lazy(() => import('pages/Login'))
+const Home = React.lazy(() => import('pages/Home'))
 
 function App() {
   return (
@@ -17,24 +22,27 @@ function App() {
     <Router history={history}>
       <div className="App">
         {/* 配置路由规则 */}
-        <Switch>
-          {/* <Route
+        {/* fallback:兜底，如果组件没有加载默认回现实fallback内容 */}
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            {/* <Route
             exact
             path="/"
             render={() => {
               return <Redirect to="/login" />
             }}
           ></Route> */}
-          <Redirect exact from="/" to="/home"></Redirect>
-          <AuthRoute path="/home" component={Home}></AuthRoute>
-          {/* <Route path="/login" component={Login}></Route> */}
-          <Route
-            path="/login"
-            render={(props) => {
-              return <Login {...props} />
-            }}
-          ></Route>
-        </Switch>
+            <Redirect exact from="/" to="/home"></Redirect>
+            <AuthRoute path="/home" component={Home}></AuthRoute>
+            {/* <Route path="/login" component={Login}></Route> */}
+            <Route
+              path="/login"
+              render={(props) => {
+                return <Login {...props} />
+              }}
+            ></Route>
+          </Switch>
+        </Suspense>
       </div>
     </Router>
   )
